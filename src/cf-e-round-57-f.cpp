a7b1b64a199a0f891976ffc6de056b6f758e3055
Yonekura <reverse.engau@gmail.com>
2019-01-06 03:01:43 +0900

    cf,agc

---
+++ src/cf-e-round-57-f.cpp
@@ -0,0 +1,159 @@
+#include "bits/stdc++.h"
+
+#define ALL(g) (g).begin(),(g).end()
+#define REP(i, x, n) for(int i = x; i < n; i++)
+#define rep(i,n) REP(i,0,n)
+#define RREP(i, x, n) for(int i = x; i >= n; i--)
+#define rrep(i, n) RREP(i,n,0)
+#define pb push_back
+#pragma GCC optimize ("-O3")
+
+using namespace std;
+
+// #define DEBUG_IS_VALID
+
+#ifdef DEBUG_IS_VALID
+#define DEB 1 
+#else
+#define DEB 0
+#endif
+#define DUMPOUT cout
+#define dump(...) if(DEB) DUMPOUT<<"  "<<#__VA_ARGS__<<" :["<<__LINE__<<":"<<__FUNCTION__<<"]"<<endl<<"    "; if(DEB) dump_func(__VA_ARGS__)
+template<typename T1,typename T2>ostream& operator << (ostream& os, pair<T1,T2> p){cout << "(" << p.first << ", " << p.second << ")"; return os;}
+template<typename T>ostream& operator << (ostream& os, vector<T>& vec) { os << "{"; for (int i = 0; i<vec.size(); i++) os << vec[i] << (i + 1 == vec.size() ? "" : ", "); os << "}"; return os; }
+template<typename T>ostream& operator << (ostream& os, set<T>& st){cout << "{"; for(auto itr = st.begin(); itr != st.end(); itr++) cout << *itr << (next(itr)!=st.end() ? ", " : ""); cout << "}"; return os;}
+template<typename T1,typename T2>ostream& operator << (ostream& os, map<T1,T2> mp){cout << "{"; for(auto itr = mp.begin(); itr != mp.end(); itr++) cout << "(" << (itr->first) << ", " << (itr->second) << ")" << (next(itr)!=mp.end() ? "," : ""); cout << "}"; return os; }
+
+void dump_func(){DUMPOUT << endl;}
+template <class Head, class... Tail>void dump_func(Head&& head, Tail&&... tail){ DUMPOUT << head; if (sizeof...(Tail) == 0) { DUMPOUT << " "; } else { DUMPOUT << ", "; } dump_func(std::move(tail)...);}
+template<class T> inline bool chmax(T& a,T const& b){if(a>=b) return false; a=b; return true;}
+template<class T> inline bool chmin(T& a,T const& b){if(a<=b) return false; a=b; return true;}
+void _main();
+int main(){ cin.tie(0); ios::sync_with_stdio(false); _main(); return 0;}
+
+using ll = long long;
+using P = pair<int,int>;
+using Pl = pair<ll,ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+
+const int mod=1e9+7,INF=1<<29;
+const double EPS=1e-12,PI=3.1415926535897932384626;
+const ll lmod = 1e9+7,LINF=1LL<<59; 
+
+template<int MOD> struct ModInt {
+    static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+    ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+    int get() const { return (int)x; }
+    ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+    ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+    ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+    ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+    ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+    ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+    ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+        while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+        return ModInt(u); }
+    bool operator==(ModInt that) const { return x == that.x; }
+    bool operator!=(ModInt that) const { return x != that.x; }
+    ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+    ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+template<typename T, int FAC_MAX> struct Comb { vector<T> fac, ifac;
+    Comb(){fac.resize(FAC_MAX,1);ifac.resize(FAC_MAX,1); for(int i = 1; i < FAC_MAX; i++)fac[i]=fac[i-1]*i;
+        ifac[FAC_MAX-1]=T(1)/fac[FAC_MAX-1];for(int i = FAC_MAX-2; i >= 1; i--)ifac[i]=ifac[i+1]*T(i+1);}
+    T aPb(int a, int b) { if (b < 0 || a < b) return T(0); return fac[a] * ifac[a - b]; }
+    T aCb(int a, int b) { if (b < 0 || a < b) return T(0); return fac[a] * ifac[a - b] * ifac[b]; }
+    T nHk(int n, int k) { if (n == 0 && k == 0) return T(1); if (n <= 0 || k < 0) return 0;
+        return aCb(n + k - 1, k); }}; // nHk = (n+k-1)Ck : n is separator
+
+using mint =  ModInt<998244353>;
+using vm = vector<mint>;
+
+template <typename T>
+class FenwickTree {
+  const int n;
+  vector<T> data;
+public:
+  FenwickTree(int n) : n(n), data(n, 0) {}
+  void add(int i, T value) {
+    for (; i < n; i |= i + 1) data[i] += value;
+  }
+  T sum(int i) const {
+    // sum of [0,i]
+    T res = 0;
+    for (; i >= 0; i = (i & (i + 1)) - 1) res += data[i];
+    return res;
+  }
+  T sum(int l, int r) const {
+    // sum of [l,r)
+    return sum(r - 1) - sum(l - 1);
+  }
+};
+
+void _main() {
+  int N; cin >> N ;
+  vi x(N); rep(i,N) cin >> x[i] ;
+  vi idx(N+1,-1);
+  mint cntb = 0;
+  rep(i,N){
+    if(x[i]==-1){
+      cntb += mint(1);
+      continue;
+    }
+    idx[x[i]] = i;
+  }
+  mint cnta = mint(N) - cntb;
+  mint aa = 0;
+  FenwickTree<mint> tree(N+1);
+  RREP(i,N,1){
+    if(idx[i]==-1) continue;
+    aa += tree.sum(0,idx[i]);
+    tree.add(idx[i],mint(1));
+  }
+  mint bb = cntb*(cntb-mint(1))/mint(4);
+  vm right(N+1),left(N+1);
+  mint cnt_space = 0;
+  RREP(i,N-1,0){
+    if(x[i]==-1){
+      cnt_space += 1;
+      continue;
+    }
+    right[x[i]] = cnt_space;
+  }
+  cnt_space = 0;
+  rep(i,N){
+    if(x[i]==-1){
+      cnt_space += 1;
+      continue;
+    }
+    left[x[i]] = cnt_space;
+  }
+  mint temp = 0, ab = 0;  
+  RREP(i,N,1){
+    if(idx[i]!=-1){
+      temp += right[i];
+      continue;
+    }
+    ab += temp;
+  }
+  temp = 0;
+  REP(i,1,N+1){
+    if(idx[i]!=-1){
+      temp += left[i];
+      continue;
+    }
+    ab += temp;
+  }
+  dump(left,right);
+  dump(aa,ab,bb);
+  ab /= cntb;
+  cout << aa+ab+bb << endl;
+}
