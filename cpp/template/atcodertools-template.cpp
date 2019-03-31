bc9723457bc74350581e89b83b9d7cdda04deaf9
Yonekura <reverse.engau@gmail.com>
2019-03-31 18:46:53 +0900

    add atcodertools-template and rm some files

---
+++ cpp/template/atcodertools-template.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(g) (g).begin(),(g).end()
+#define REP(i, x, n) for(int i = x; i < n; i++)
+#define rep(i,n) REP(i,0,n)
+#define RREP(i, x, n) for(int i = x; i >= n; i--)
+#define rrep(i, n) RREP(i,n,0)
+#define pb push_back
+#pragma GCC optimize ("-O3")
+
+using namespace std;
+
+#define DUMPOUT cout
+#define dump(...) if(DEB) DUMPOUT<<"  "<<#__VA_ARGS__<<" :["<<__LINE__<<":"<<__FUNCTION__<<"]"<<endl<<"    "; if(DEB) dump_func(__VA_ARGS__)
+template<typename T1,typename T2>ostream& operator << (ostream& os, pair<T1,T2> p){cout << "(" << p.first << ", " << p.second << ")"; return os;}
+template<typename T>ostream& operator << (ostream& os, vector<T>& vec) { os << "{"; for (int i = 0; i<vec.size(); i++) os << vec[i] << (i + 1 == vec.size() ? "" : ", "); os << "}"; return os; }
+template<typename T>ostream& operator << (ostream& os, set<T>& st){cout << "{"; for(auto itr = st.begin(); itr != st.end(); itr++) cout << *itr << (next(itr)!=st.end() ? ", " : ""); cout << "}"; return os;}
+template<typename T1,typename T2>ostream& operator << (ostream& os, map<T1,T2> mp){cout << "{"; for(auto itr = mp.begin(); itr != mp.end(); itr++) cout << "(" << (itr->first) << ", " << (itr->second) << ")" << (next(itr)!=mp.end() ? "," : ""); cout << "}"; return os; }
+
+void dump_func(){DUMPOUT << endl;}
+template <class Head, class... Tail>void dump_func(Head&& head, Tail&&... tail){ DUMPOUT << head; if (sizeof...(Tail) == 0) { DUMPOUT << " "; } else { DUMPOUT << ", "; } dump_func(std::move(tail)...);}
+template<class T> inline bool chmax(T& a,T const& b){if(a>=b) return false; a=b; return true;}
+template<class T> inline bool chmin(T& a,T const& b){if(a<=b) return false; a=b; return true;}
+
+{% if mod %}
+const long long MOD = {{ mod }};
+{% endif %}
+{% if yes_str %}
+const string YES = "{{ yes_str }}";
+{% endif %}
+{% if no_str %}
+const string NO = "{{ no_str }}";
+{% endif %}
+
+{% if prediction_success %}
+void solve({{ formal_arguments }}){
+
+}
+{% endif %}
+
+int main(){
+  {% if prediction_success %}
+  {{input_part}}
+  solve({{ actual_arguments }});
+  {% else %}
+  // Failed to predict input format
+  {% endif %}
+  return 0;
+}
