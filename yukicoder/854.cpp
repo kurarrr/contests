4c73eb09c4a62c30782c6d83e2f6835b6de0c903
kurarrr <reverse.engau@gmail.com>
2019-07-27 16:48:27 +0900

    fix prime and yukicoder

--- yukicoder/854.cpp
+++ yukicoder/854.cpp
@@ -1,4 +1,13 @@
-#include "bits/stdc++.h"
+#ifdef DEBUG_IS_VALID
+#define DEB 1 
+#define _LIBCPP_DEBUG 0
+#else
+#define DEB 0
+#define NDEBUG
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
 
 #define ALL(g) (g).begin(),(g).end()
 #define REP(i, x, n) for(int i = x; i < n; i++)
@@ -6,28 +15,29 @@
 #define RREP(i, x, n) for(int i = x; i >= n; i--)
 #define rrep(i, n) RREP(i,n,0)
 #define pb push_back
+#define fi first
+#define se second
 #pragma GCC optimize ("-O3")
 
 using namespace std;
 
-#define DEBUG_IS_VALID
-
-#ifdef DEBUG_IS_VALID
-#define DEB 1 
-#else
-#define DEB 0
-#endif
 #define DUMPOUT cout
 #define dump(...) if(DEB) DUMPOUT<<"  "<<#__VA_ARGS__<<" :["<<__LINE__<<":"<<__FUNCTION__<<"]"<<endl<<"    "; if(DEB) dump_func(__VA_ARGS__)
 template<typename T1,typename T2>ostream& operator << (ostream& os, pair<T1,T2> p){cout << "(" << p.first << ", " << p.second << ")"; return os;}
 template<typename T>ostream& operator << (ostream& os, vector<T>& vec) { os << "{"; for (int i = 0; i<vec.size(); i++) os << vec[i] << (i + 1 == vec.size() ? "" : ", "); os << "}"; return os; }
-template<typename T>ostream& operator << (ostream& os, set<T>& st){for(auto itr = st.begin(); itr != st.end(); itr++) cout << *itr << (next(itr)!=st.end() ? ", " : ""); cout << "}"; return os;}
+template<typename T>ostream& operator << (ostream& os, set<T>& st){cout << "{"; for(auto itr = st.begin(); itr != st.end(); itr++) cout << *itr << (next(itr)!=st.end() ? ", " : ""); cout << "}"; return os;}
 template<typename T1,typename T2>ostream& operator << (ostream& os, map<T1,T2> mp){cout << "{"; for(auto itr = mp.begin(); itr != mp.end(); itr++) cout << "(" << (itr->first) << ", " << (itr->second) << ")" << (next(itr)!=mp.end() ? "," : ""); cout << "}"; return os; }
 
 void dump_func(){DUMPOUT << endl;}
 template <class Head, class... Tail>void dump_func(Head&& head, Tail&&... tail){ DUMPOUT << head; if (sizeof...(Tail) == 0) { DUMPOUT << " "; } else { DUMPOUT << ", "; } dump_func(std::move(tail)...);}
 template<class T> inline bool chmax(T& a,T const& b){if(a>=b) return false; a=b; return true;}
 template<class T> inline bool chmin(T& a,T const& b){if(a<=b) return false; a=b; return true;}
+void solve();
+int main(void) {
+  std::cout << std::fixed << std::setprecision(15);
+  solve();
+	return 0;
+}
 
 using ll = long long;
 using P = pair<int,int>;
@@ -36,17 +46,15 @@ using vi = vector<int>;
 using vvi = vector<vi>;
 using vl = vector<ll>;
 using vvl = vector<vl>;
+using vp = vector<Pl>;
+using vvp = vector<vp>;
 
-const int mod=1e9+7,INF=1<<29;
-const double EPS=1e-12,PI=3.1415926535897932384626;
-const ll lmod = 1e9+7,LINF=1LL<<59; 
+const int INF = 1<<29;
+const long long LINF=1LL<<59;
 
-
-// 素数
 vector<ll> primes(ll n) {
-  // enumerate primes less than or equal to n
-  // complexity: O(n loglog(n))
-  // res.size() ~ O(n/log(n))
+  // O(n loglog(n))
+  // res: sz ~ O(n/log(n))
   vector<bool> is_prime(n+1,true);
   is_prime[0] = is_prime[1] = false;
   vector<ll> res;
@@ -57,53 +65,55 @@ vector<ll> primes(ll n) {
   }
   return res;
 }
+map<int,int> mp; // key: prime, val: index
+ll sz = 0;
 
-// 約数の列挙
-vector<ll> divisors(ll n) {
-  vector<ll> res;
-  for(int i=1; i*i<=n; i++) {
-    if(n % i == 0) {
-      res.push_back(i);
-      if(n / i != i) res.push_back(n/i);
-    }
-  }
-  return res;
+vl enumpr(ll n) {
+  // assert(sz != 0);
+  vl vec(sz+1);
+  for (ll i = 2; 1LL * i*i <= n; i++) while (n%i == 0) vec[mp[i]]++, n /= i;
+  if (n>1) vec[mp[n]]++;
+  return vec;
 }
 
-// 素因数の列挙
-vector<ll> prime_factors(ll n) {
-  vector<ll> res;
-  for(ll i=2; i*i<=n; i++) {
-    if(n % i == 0) {
-      res.push_back(i);
-      while(n % i == 0) n /= i;
-    }
+void solve(){
+  ll N;
+  cin >> N ;
+  vl a(N);
+  rep(i,N) cin >> a[i] ;
+  auto pr = primes(2000);
+  sz = pr.size();
+  rep(i,pr.size()) mp[pr[i]] = i;
+  vvl s(N+1,vl(sz+1));
+  rep(i,N){
+    s[i+1] = enumpr(a[i]);
+    if(a[i]==0) s[i+1][sz]++;
   }
-  if(n != 1) res.push_back(n);
-  return res;
-}
-
-void _main() {
-  int t; cin >> t ;
-  auto p = primes(ll(1e3+1));
-  rep(i,t){
-    ll n; cin >> n ;
-    vl ans;
-    int sz = min(int(sqrt(double(n)))+1,int(p.size()));
-    rep(j,sz) REP(k,j,sz){
-      ll s = p[j];
-      ll t = p[k];
-      ll temp2 = n-ll(s*s)-ll(t*t);
-      ll temp = ll(sqrt(double(temp2)));
-      if(temp*temp!=temp2) continue;
-      if(!is_prime[temp]) continue;
-      if(!(s*s+t*t+temp*temp==n)) continue;
-      vl x = {s,t,temp};
-      sort(ALL(x));
-      ans.pb(x[0]*ll(1e6)+x[1]*ll(1e3)+x[2]);
+  rep(i,N){
+    rep(j,sz+1) s[i+1][j] += s[i][j];
+  }
+  int Q;
+  cin >> Q ;
+  rep(q,Q){
+    ll p; ll l,r;
+    cin >> p >> l >> r ;
+    l--; r--;
+    if(s[r+1][sz] - s[l][sz] > 0 ){
+      cout << "Yes" << endl;
+      continue;
     }
-    sort(ALL(ans));
-    ans.erase(unique(ALL(ans)),ans.end());
-    cout << ans.size() << endl;
+    bool ok = true;
+    rep(j,sz){
+      int cnt = 0;
+      while(p % pr[j] == 0){
+        p /= pr[j]; cnt++;
+      }
+      if(s[r+1][j] - s[l][j] < cnt){
+        ok = false;
+        break;
+      }
+    }
+    ok &= (p <= 2000);
+    cout << (ok ? "Yes" : "NO") << endl;
   }
 }
